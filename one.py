1bb971a3ad386bf9438e59edd9708b8aef895803
Alejo <acelis@unal.edu.co>
2022-07-27 19:16:46 -0500

    Primer_Ejercicio

---
+++ one.py
@@ -0,0 +1,12 @@
+# Cree un programa que, al recibir como datos dos números enteros, calcule la
+# suma, resta y multiplicación de dichos numeros.
+
+n1=float(input("ingrese el primer numero: "))
+n2=float(input("ingrese el segundo numero: "))
+
+suma=n1+n2
+print("la suma es: ",suma)
+resta=n1-n2
+print("la resta es: ",resta)
+multi=n1*n2
+print("la multiplicación es: ",multi)
